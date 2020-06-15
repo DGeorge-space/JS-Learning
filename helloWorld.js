287af17e5940d2c1cd480137270eda46c1d64194
Daniel George <danieljamesgeorge@gmail.com>
2020-06-15 17:47:29 +0100

    first commit

---
+++ helloWorld.js
@@ -0,0 +1,17 @@
+//JS is dynamic weakly types, runtime compiler. 
+//nothing to do with java
+
+// client sends request to server, server responds to client with a response 
+// a simple response is html 
+
+//js skips subsequent requests by changing the existing page. compiled at runtime (unlike c++)
+
+//can change type of a variable dynamically- as datatypes are assumed
+
+// host environment: (not necessarily just the browser) eg server side
+
+// can send background http requests (ajax) but js can't interact with os - node.js is an extracted chrome v8 engine which can be used on the server side
+
+// node.js can access the local files/ os, but can't manipulate HTML/ CSS
+
+// 3 main sections for course: basics, strong foundation, advanced concepts:
